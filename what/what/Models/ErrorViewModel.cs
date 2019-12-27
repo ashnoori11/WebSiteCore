44d6893dfaf9eefdfeb9d1744dfd7d11071e2b04
unknown <ashnoori11@gmail.com>
2019-12-27 20:42:20 +0330

    initial

---
+++ what/what/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace what.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
